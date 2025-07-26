fcf3962d8e791a5e5a783f39bd5294a052a84ca2
Huy-pynok3 <huy53497@gmail.com>
2025-07-26 15:45:36 +0700

    Create My Bookings

--- client/src/pages/MyBooking.jsx
+++ client/src/pages/MyBooking.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import { assets, dummyMyBookingsData } from "@/assets/assets";
+import Title from "@/components/Title";
+import React, { useEffect, useState } from "react";
 
 const MyBooking = () => {
-  return (
-    <div>MyBooking</div>
-  )
-}
+    const [bookings, setBookings] = useState([]);
+    const currency = import.meta.env.VITE_CURRENCY;
+    const fetchBooking = async () => {
+        setBookings(dummyMyBookingsData);
+    };
 
-export default MyBooking
+    useEffect(() => {
+        fetchBooking();
+    }, []);
+
+    return (
+        <div className="px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl">
+            <Title title="My Booking" subTitle="View and manage your all car bookings" align="left" />
+            
+            <div>
+                {bookings.map((booking, i) => (
+                    <div
+                        key={booking._id}
+                        className="grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor rounded-lg mt-5 first:mt-12"
+                    >
+                        {/* Car img + info */}
+                        <div className="md:col-span-1">
+                            <div className="rounded-md overflow-hidden mb-3">
+                                <img
+                                    src={booking.car.image}
+                                    className="w-full h-auto aspect-video object-cover"
+                                    alt=""
+                                />
+                            </div>
+                            <p className="text-lg font-medium mt-2">
+                                {booking.car.brand} {booking.car.model}
+                            </p>
+                            <p className="text-gray-500">
+                                {booking.car.year} • {booking.car.category} • {booking.car.location}
+                            </p>
+                        </div>
+                        {/* Booking info */}
+                        <div className="md:col-span-2">
+                            <div className="flex items-center gap-2">
+                                <p className="px-3 py-1.5 bg-light rounded">Booking #{i +1}</p>
+                                <p className={`px-3 py-1 text-xs rounded-full ${booking.status === "confirmed" ? "bg-green-400/15 text-green-600" : "bg-red-400/15 text-red-600"}`}>{booking.status}</p>
+                            </div>
+
+                            <div className="flex items-start gap-2 mt-3">
+                                <img src={assets.calendar_icon_colored} className="w-4 h-4 mt-1" alt="" />
+                                <div>
+                                    <p class="text-gray-500">Rental Period</p>
+                                    <p>
+                                        {new Date(booking.pickupDate).toLocaleDateString()} -{" "}
+                                        {new Date(booking.returnDate).toLocaleDateString()}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="flex items-start gap-2 mt-3">
+                                <img src={assets.location_icon_colored} className="w-4 h-4 mt-1" alt="" />
+                                <div>
+                                    <p className="text-gray-500">Pick-up Location</p>
+                                    <p>{booking.car.location}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="md:col-span-1 flex flex-col justify-between gap-6">
+                            <div className="text-sm text-gray-500 text-right">
+                                <p>Total Price</p>
+                                <h1 class="text-2xl font-semibold text-primary">{currency} {booking.price}</h1>
+                                <p>Booked on {new Date(booking.pickupDate).toLocaleDateString()}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default MyBooking;
